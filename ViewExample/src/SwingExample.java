208db925937910a8e7b6c444dd323ef7e78c4493
BRUNO <brunoovedoveto@gmail.com>
2016-06-03 19:41:26 -0300

    Creating project. Example for Java Swing

---
+++ ViewExample/src/SwingExample.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Bruno on 03/06/2016.
+ */
+public class SwingExample {
+    public static void main(String [] args){
+        JFrame frame = new JFrame();
+    }
+}
